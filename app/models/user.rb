f1885a9ac2b0c35f6669f2341e1ebd60712d90ad
NaotoToriizuka <toriizuka.com@gmail.com>
2021-02-23 21:35:50 +0900

    Userモデル　バリデーション　設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments
+
+  validates :nicname, precence: true, length: { maximum: 6 }
 end
